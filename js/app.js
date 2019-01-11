4a429009d7ec0a4bed0138fe1c251c2f14e0d496
Collin McMahon <collin@Collins-MacBook-Pro.local>
2019-01-10 21:54:42 -0500

    nav

--- js/app.js
+++ js/app.js
@@ -2,3 +2,7 @@ $(document).foundation()
 
 $("[data-menu-underline-from-center] a").addClass("underline-from-center");
 
+// $('[data-toggle-menu]').on("click", function(){
+//   $("#overlay-nav-menu").toggleClass("is-open");
+// });
+
