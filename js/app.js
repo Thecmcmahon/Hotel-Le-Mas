71c0a840596af73b53f2f3a562e666d49c4750dd
Collin McMahon <collin@Collins-MacBook-Pro.local>
2019-01-12 16:50:07 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,16 +2,18 @@ $(document).foundation()
 
 $("[data-menu-underline-from-center] a").addClass("underline-from-center");
 
-// $(function() {
-//   $(window).scroll(function() {
-//     var winTop = $(window).scrollTop();
-//     if (winTop >= 30) {
-//       $("header").addClass("shrink");
-//     } else{
-//       $("header").removeClass("shrink");
-//     }
-//   });
-// });
+$(function() {
+  $(window).scroll(function() {
+    var winTop = $(window).scrollTop();
+    if (winTop >= 30) {
+      $("header").addClass("shrink");
+      $("header h1").addClass("shrink2");
+    } else{
+      $("header").removeClass("shrink");
+      $("header h1").removeClass("shrink2");
+    }
+  });
+});
 
 
 // $(function() {
