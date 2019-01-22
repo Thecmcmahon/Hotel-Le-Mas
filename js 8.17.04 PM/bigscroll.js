b11bb94e691e82b5b9a3e1ec92e6cffa83179e4e
Collin McMahon <collin@Collins-MacBook-Pro.local>
2019-01-21 20:17:43 -0500

    lots

---
+++ js 8.17.04 PM/bigscroll.js
@@ -0,0 +1,28 @@
+
+$(document).foundation()
+  
+  $(function myFunctionBig(){
+    if( $(window).width() <= 640 ) {
+        console.log("the width is" + $(window).width());
+        $.scrollify({
+    section : ".spacing",
+    sectionName : "section-name",
+    interstitialSection : "",
+    easing: "easeOutExpo",
+    scrollSpeed: 1000,
+    offset : -150,
+    scrollbars: true,
+    standardScrollElements: "",
+    setHeights: false,
+    overflowScroll: true,
+    updateHash: true,
+    touchScroll:true,
+    before:function() {},
+    after:function() {},
+    afterResize:function() {},
+    afterRender:function() {}
+  });
+    console.log("small screen");
+  }
+});
+
